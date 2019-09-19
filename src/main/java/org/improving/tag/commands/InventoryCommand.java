4773a649b315fdcc9c19578ea859009d6d57a7df
Jennier Granados <Jennier.Granados@improving.com>
2019-09-19 11:54:05 -0500

    Created all commands and created constructors

---
+++ src/main/java/org/improving/tag/commands/InventoryCommand.java
@@ -0,0 +1,8 @@
+package org.improving.tag.commands;
+
+public class InventoryCommand extends BaseEmoteCommand{
+    public InventoryCommand() {
+        super ("inventory" , "You are carrying nothing");
+    }
+
+}
