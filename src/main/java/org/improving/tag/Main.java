98dbdc83b25847fe77c28f23f4ae578af4805a36
Jennier Granados <Jennier.Granados@improving.com>
2019-09-19 10:08:20 -0500

    Constructors and person

--- src/main/java/org/improving/tag/Main.java
+++ src/main/java/org/improving/tag/Main.java
@@ -2,32 +2,18 @@ package org.improving.tag;
 
 import java.util.Scanner;
 
+
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        boolean loop = true;
-        while(loop) {
-            System.out.print("> ");
-            String input = scanner.nextLine();
-            else if(input.trim().equals("look")) {
-                System.out.println("You look around");
-            }
-            else if (input.trim().equals("inventory")) {
-                System.out.println("You are carrying nothing.");
-            }
-            else if (input.trim().equals("dance")) {
-                System.out.println("You dance around.");
-            }
-            else if (input.trim().equals("jump")) {
-                System.out.println("You jump in the air.");
-            }
-            else if (input.trim().equals("exit")) {
-                System.out.println("Goodbye.");
-                loop = false;
-            } else {
-                System.out.println("huh? I don't understand.");
-            }
+        System.out.println("Start of Main");
+    Game game = new Game(100, 45);
+        System.out.println("Declared Game");
+    game.run();
+        System.out.println("After Run()");
 
-        }
+        long elapsedTicks = game.getEndTime().getTime() -
+                        game.getStartTime().getTime();
+        double elapsedSeconds = elapsedTicks / 1000.0;
+        System.out.println("We were running for " + elapsedSeconds + "s.");
     }
 }
